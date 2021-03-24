06a91b94a51476d13aa77e44319e1fd4264021a5
Pat Smith <patsmith@gmail.com>
2021-03-24 10:43:52 -0400

    made corrections to the Program.cs file.

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -7,13 +7,20 @@ namespace BankEncapsulation
         static void Main(string[] args)
         {
             // Created the BankAccount class
-            public class BankAccount()
-            {
+            var account = new BankAccount();
+            
+            // Getting the amount to deposit into account
+            Console.WriteLine("What is your deposit amount?");
+            
+            // Creating a var to hold the amount of the deposit once it is read
+            var amountToDeposit = double.Parse(Console.ReadLine());
 
-            }
+            // Gets the amount to deposit and puts it into Deposit
+            account.Deposit(amountToDeposit);
+            
+            // Tells the account holder the balance of their account after deposit
+            Console.WriteLine($"Your current balance is {account.GetBalance()}");
 
         }
-
     }
-
 }
