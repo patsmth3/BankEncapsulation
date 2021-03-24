db20039849ea2c29dd3fdf67891837c8e23876e1
Pat Smith <patsmith@gmail.com>
2021-03-24 12:58:14 -0400

    format changes

--- BankEncapsulation/BankAccount.cs
+++ BankEncapsulation/BankAccount.cs
@@ -25,7 +25,5 @@ namespace BankEncapsulation
         {
             return _balance;
         }
-       
-        
     }
 }
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -6,10 +6,10 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
-            // Created the BankAccount class
+            // Created a new instance of the BankAccount class
             var account = new BankAccount();
             
-            // Getting the amount to deposit into account
+            // Gets the amount to deposit into account
             Console.WriteLine("What is your deposit amount?");
             
             // Creating a var to hold the amount of the deposit once it is read
@@ -20,7 +20,6 @@ namespace BankEncapsulation
             
             // Tells the account holder the balance of their account after deposit
             Console.WriteLine($"Your current balance is {account.GetBalance()}");
-
         }
     }
 }
